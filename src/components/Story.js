fe3ebe98b35b3bd8ecf26c7ece51988d978e342f
amoghwagh <amoghwagh@gmail.com>
2019-10-01 19:44:48 +0530

    Added Upvote and downvote css upon click

--- src/components/Story.js
+++ src/components/Story.js
@@ -2,22 +2,49 @@ import React, { Component } from "react";
 
 class Story extends Component {
   state = {
+    upVoteStatus: "",
+    downVoteStatus: "",
     score: this.props.score,
     upvoteState: false,
     downvoteState: false
   };
+
+  upVoteClick = () => {
+    this.setState({
+      upVoteStatus: "green-text",
+      downVoteStatus: ""
+    });
+  };
+
+  downVoteClick = () => {
+    this.setState({
+      downVoteStatus: "red-text",
+      upVoteStatus: ""
+    });
+  };
+
   render() {
     return (
       <li className="collection-item">
-        <a className="upvote btn-floating btn-small waves-effect waves-light grey darken-2">
-          <i className="material-icons green-text">arrow_drop_up</i>
+        <a
+          className="upvote btn-floating btn-small waves-effect waves-light grey darken-2"
+          onClick={this.upVoteClick.bind(this)}
+        >
+          <i className={"material-icons " + this.state.upVoteStatus}>
+            arrow_drop_up
+          </i>
         </a>
         <a href={this.props.url}>
           <span className="title"> {this.props.title} </span>
         </a>
         <br />
-        <a className="downvote btn-floating btn-small waves-effect waves-light grey darken-2">
-          <i className="material-icons red-text">arrow_drop_down</i>
+        <a
+          className="downvote btn-floating btn-small waves-effect waves-light grey darken-2"
+          onClick={this.downVoteClick}
+        >
+          <i className={"material-icons " + this.state.downVoteStatus}>
+            arrow_drop_down
+          </i>
         </a>
         <span className="tags">
           <span> {this.state.score} points by </span>
