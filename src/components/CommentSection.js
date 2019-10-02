c2a76499b333a45806180a6fc5977fc1f8f4af1c
amoghwagh <amoghwagh@gmail.com>
2019-10-03 02:10:09 +0530

    Added mock counter structure

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -2,29 +2,43 @@ import React, { Component } from "react";
 
 class Comments extends Component {
   renderComment = text => {
-    return <li class="collection-item white comment-text">{text}</li>;
+    return (
+      <li class="collection-item white comment-text">
+        <div class="counter-section">
+          {" "}
+          <a href="javascript:void(0)" className="upvote">
+            <i className={"material-icons "}>arrow_drop_up</i>
+          </a>
+          <span>0</span>
+          <a href="javascript:void(0)" className="downvote">
+            <i className={"material-icons "}>arrow_drop_down</i>
+          </a>
+        </div>
+        <span>{text}</span>
+      </li>
+    );
   };
   render() {
     return (
-      <div class="comment-section row">
-        <div class="col s12 m6">
-          <div class="card white">
-            <div class="card-content white-text">
-              <div class="row">
-                <div class="row">
-                  <div class="input-field col s12">
+      <div className="comment-section row">
+        <div className="col s12 m6">
+          <div className="card white">
+            <div className="card-content white-text">
+              <div className="row">
+                <div className="row">
+                  <div className="input-field col s12">
                     <textarea
                       id="textarea"
-                      class="materialize-textarea"
+                      className="materialize-textarea"
                     ></textarea>
                     <label for="textarea">New Comment</label>
                   </div>
-                  <a class="waves-effect waves-light btn">
-                    <i class="material-icons right">add</i>Add New Comment
+                  <a className="waves-effect waves-light btn">
+                    <i className="material-icons right">add</i>Add New Comment
                   </a>
                 </div>
-                <div class="row">
-                  <ul class="comment-box collection black-text">
+                <div className="row">
+                  <ul className="comment-box collection black-text">
                     {this.props.story.comments.map(comment =>
                       this.renderComment(comment.text)
                     )}
